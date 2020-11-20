022fdba7253350f88544fc67473cbaf46b993301
tomek-skrond <tomek2.skrond@gmail.com>
2020-11-20 13:00:28 +0100

    added Organism class

---
+++ src/main/java/Bootstrap/Bootstrap.java
@@ -0,0 +1,14 @@
+package Bootstrap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class Bootstrap{
+
+    public Bootstrap(){
+        System.out.println("konstruktor bootstrap");
+    }
+}
---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+import Bootstrap.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String [] args){
+
+        Bootstrap b = new Bootstrap();
+
+    }
+}
