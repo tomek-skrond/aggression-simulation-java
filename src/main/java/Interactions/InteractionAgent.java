f4ac8a06db6801f73be77aa105ac9f78e60af9b9
tomek-skrond <tomek2.skrond@gmail.com>
2021-01-06 20:23:58 +0100

    functions and fields in InteractionAgent

--- src/main/java/Interactions/InteractionAgent.java
+++ src/main/java/Interactions/InteractionAgent.java
@@ -1,4 +1,21 @@
 package Interactions;
 
+import StructureOrganisation.Batch;
+import StructureOrganisation.StructureOrganisationAgent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InteractionAgent {
+    StructureOrganisationAgent structureAgent;
+    List<Batch> batches;
+
+    public InteractionAgent(ArrayList<Batch> _batches,StructureOrganisationAgent _structureAgent){
+        this.structureAgent = _structureAgent;
+        this.batches = _batches;
+    }
+
+    void InteractionLoop(){
+
+    }
 }
